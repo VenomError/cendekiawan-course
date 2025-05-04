ca95b592988ed4c5eff45307d699dc384924baef
VenomError <studyweb2023@gmail.com>
2025-05-04 13:23:07 +0000

    May 4, 2025, 9:23 PM

---
+++ app/Livewire/Landing/Kursus/BookingKursus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Landing\Kursus;
+
+use Livewire\Component;
+
+class BookingKursus extends Component
+{
+    public function render()
+    {
+        return view('livewire.landing.kursus.booking-kursus');
+    }
+}
