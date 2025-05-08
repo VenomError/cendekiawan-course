57eed121e68d3a2ef2aaa8f417552f325064338d
VenomError <studyweb2023@gmail.com>
2025-05-09 06:31:28 +0800

    `Added eager loading for pendaftar relationships and formatted date display`

--- resources/views/livewire/landing/pendaftar/riwayat.blade.php
+++ resources/views/livewire/landing/pendaftar/riwayat.blade.php
@@ -4,28 +4,34 @@
         <div class="container">
             <div class="row justify-content-center">
                 @foreach ($riwayats as $pendaftar)
+                @php
+                    $pendaftar->load(['jadwals','kursuses']);
+                @endphp
+                
                     <div class="col-xl-8 col-lg-7 mb-4">
+                        
                         <div class="row">
                             <div
                                 class="col-xl-12 col-lg-12 col-md-12 wow fadeInUp"
                                 data-wow-delay="200ms"
                                 style="visibility: hidden; animation-delay: 200ms; animation-name: none;"
                             >
+                            
                                 <div class="blog-two__item blog-two__item--list">
 
                                     <div class="blog-two__content">
                                         <div class="blog-two__top-meta">
                                             <div class="blog-two__cats">
-                                                <a>{{ $pendaftar->status }}</a>
+                                                <a class="text-white">{{ $pendaftar->status }}</a>
                                             </div>
                                             <div class="blog-two__date">
                                                 <i class="icon-clock"></i>
-                                                {{ $pendaftar->jadwals->first()->start_datetime?->format('Y-m-d h:i a') }}
+                                                {{ $pendaftar->jadwals->first()?->value('start_datetime')?->format('D d M, Y h:i a') }}
                                             </div>
                                         </div>
                                         <h3 class="blog-two__title">
                                             <a href="blog-details.html">
-                                                {{ $pendaftar->kursuses->first()->name }}
+                                               {{ $pendaftar->kursuses->first()?->value('name') }}
                                             </a>
                                         </h3><!-- /.blog-title -->
                                      
