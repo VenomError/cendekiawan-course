46d9cd73a8f4845b6d89ed2b8fe455f86ba35906
VenomError <venoms00001@gmail.com>
2025-07-25 03:37:44 +0800

    feat: enhance CreateKursusForm with new properties and validation rules

--- app/Livewire/Forms/Kursus/CreateKursusForm.php
+++ app/Livewire/Forms/Kursus/CreateKursusForm.php
@@ -1,40 +1,62 @@
 <?php
+
 namespace App\Livewire\Forms\Kursus;
 
 use App\Models\Kursus;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
-use Livewire\Attributes\Validate;
 use Livewire\Form;
 use Livewire\WithFileUploads;
 
 class CreateKursusForm extends Form
 {
     use WithFileUploads;
+
     public $name;
+
     public $price;
+
     public $hour_duration;
+
     public $description;
+
     public $thumbnail;
+
+    /**
+     * teacher_name
+     *
+     * @var mixed
+     */
     public $teacher_name;
+
+    /**
+     * teacher_foto
+     *
+     * @var mixed
+     */
     public $teacher_foto;
+
     public array $benefits = [];
 
+    /**
+     * rules
+     *
+     * @return void
+     */
     public function rules()
     {
         return [
-            'name'          => 'required|unique:kursuses,name',
-            'price'         => 'required|numeric|min:1',
+            'name' => 'required|unique:kursuses,name',
+            'price' => 'required|numeric|min:1',
             'hour_duration' => 'required|numeric|min:1',
-            'description'   => 'required',
-            'benefits'      => 'required|array'
+            'description' => 'required',
+            'benefits' => 'required|array',
         ];
     }
 
     public function create()
     {
-        try
-        {
+        try {
             $this->validate();
 
             return DB::transaction(function () {
@@ -46,6 +68,7 @@ class CreateKursusForm extends Form
 
                 flash()->success('Create new Kursus Success');
                 $this->reset();
+
                 return $kursus;
             });
         } catch (ValidationException $th) {
@@ -53,6 +76,7 @@ class CreateKursusForm extends Form
             throw $th;
         } catch (\Throwable $th) {
             flash()->error('Failed to Create Kursus');
+
             return false;
         }
     }
@@ -62,10 +86,10 @@ class CreateKursusForm extends Form
 
         $this->thumbnail = $thumbnail->storePublicly(path: 'kursus/thumbnail', options: 'public');
     }
+
     public function uploadTeacherFoto($teacherFoto)
     {
 
         $this->teacher_foto = $teacherFoto->storePublicly(path: 'kursus/teacher-foto', options: 'public');
     }
-
 }
--- app/Livewire/Jadwal/EditJadwal.php
+++ app/Livewire/Jadwal/EditJadwal.php
@@ -24,6 +24,7 @@ class EditJadwal extends Component
 
     }
 
+
     public function render()
     {
         return view('livewire.jadwal.edit-jadwal');
